4851da7e7f9f98992d1dba0f9bb403b0353249fd
cleverton <cleverton.lima@serpro.gov.br>
2019-11-25 16:43:49 -0200

    Uma nova classe para criar objetos bicicleta

---
+++ Deitel/src/main/java/SetterAndGetter/Bike.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SetterAndGetter;
+
+/**
+ *
+ * @author 03475300958
+ */
+public class Bike {
+    
+}
