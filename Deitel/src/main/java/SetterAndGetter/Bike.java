8427669ee034a3b230f685bd27b1a47ecb1b9453
cleverton <cleverton.lima@serpro.gov.br>
2019-11-25 16:48:49 -0200

    Adicionado metodo construtor e getter e setters

--- Deitel/src/main/java/SetterAndGetter/Bike.java
+++ Deitel/src/main/java/SetterAndGetter/Bike.java
@@ -11,4 +11,50 @@ package SetterAndGetter;
  */
 public class Bike {
     
+    private int aro;
+    private String cor;
+    private String Marca;
+    private Double valor;
+    
+
+    public Bike(int aro, String cor, String Marca, Double valor) {
+        this.aro = aro;
+        this.cor = cor;
+        this.Marca = Marca;
+        this.valor = valor;
+    }
+
+    public int getAro() {
+        return aro;
+    }
+
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+    
 }
