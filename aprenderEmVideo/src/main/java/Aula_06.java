e5238d98a3a0d7dded551e0dfb00629251935cbb
cleverton <cleverton.lima@serpro.gov.br>
2019-11-26 18:05:52 -0200

    Criado classes e interface controle remoto

---
+++ aprenderEmVideo/src/main/java/Aula_06.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 03475300958
+ */
+public class Aula_06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
